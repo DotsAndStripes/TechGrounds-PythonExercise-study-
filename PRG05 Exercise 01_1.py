be725719280ef1d1e954696714a16b5bda5e21aa
Marieke vd Heuvel <92033155+DotsAndStripes@users.noreply.github.com>
2021-11-07 22:32:51 +0100

    Update and rename PRG05Excersise 01.py to PRG05 Exercise 01_1.py

--- PRG05 Exercise 01_1.py
+++ PRG05 Exercise 01_1.py
@@ -1,3 +1,7 @@
+# With the command Input, a user can be asked to give a name (for instance).
+# With the loop " if" , the correct name can be filtered. 
+
+
 a = input("input your name: ")
 
 if a == "Fadi":
@@ -6,5 +10,6 @@ elif a == "John":
   print("Your name is John")
 else:
   print("Your name is not Fadi")
+ 
 
   
