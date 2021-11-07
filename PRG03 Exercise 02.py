e481cc1601c0abd6ead757930feaafd06d6be2e7
Marieke vd Heuvel <92033155+DotsAndStripes@users.noreply.github.com>
2021-11-07 21:35:47 +0100

    Update PRG03 Exercise 02.py

--- PRG03 Exercise 02.py
+++ PRG03 Exercise 02.py
@@ -1,5 +1,8 @@
+# In deze opdracht kan via command input, een zin worden geschreven met de gegeven input van de gebruiker als antwoord op een vraag.
+
 dier=input("Wat is je lievelingsdier?")
 print("Je lievelingsdier is: " +(dier))
+
 #hier is twee keer gebruik gemaakt van haakjes (())
 #bij PRINT altijd de waarde/value tussen () gezet moet worden. 
 # Dier is een value en dus wordt deze alleen geprint als deze tussen haakjes staat.
