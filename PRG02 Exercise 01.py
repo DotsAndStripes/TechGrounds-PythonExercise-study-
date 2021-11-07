eab6a5ca50663da7b6ddfecca80020035a411c8c
Marieke vd Heuvel <92033155+DotsAndStripes@users.noreply.github.com>
2021-11-07 22:26:20 +0100

    Update PRG02 Exercise 01.py

--- PRG02 Exercise 01.py
+++ PRG02 Exercise 01.py
@@ -1,4 +1,6 @@
-# In deze opdracht wordt getoond hoe de uitkomt van een formule geschreven en geprint kan worden.
+# This exercise shows how a formula can be created and how the outcome can be printed.
+# Because it is a value and not a text, the two " do not have to be used.
+# But.... the 2 () must be used else it will not work.
 
 x=5000000
 y=10000000
