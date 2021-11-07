86cfa1d7a36296cf13b6f29d60206dc843ffdb46
Marieke vd Heuvel <92033155+DotsAndStripes@users.noreply.github.com>
2021-11-07 23:46:33 +0100

    Add files via upload

---
+++ Opdracht 7 ex 1.py
@@ -0,0 +1,7 @@
+# By creating a list, and using a " for i " loop, 
+# each value (or name in this example) will be printed on a new line if the command print is used,
+# in combination with (i).
+
+name_list=["Victor" ,"Ahmad" ,"Fadi" ,"Casper" ,"Marieke"]
+for i in name_list:
+    print (i)
