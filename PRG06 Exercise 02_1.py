83e9705d50596b561c2324109b3b4ddd0b043895
Marieke vd Heuvel <92033155+DotsAndStripes@users.noreply.github.com>
2021-11-07 23:13:32 +0100

    Update and rename opdracht 6_2.py to PRG06 Exercise 02_1.py

--- PRG06 Exercise 02_1.py
+++ PRG06 Exercise 02_1.py
@@ -1,4 +1,4 @@
-# With the command def, a new function is defined. 
+# With the command def, a new function can be defined. 
 # The two () have to be written down behind myfunction, else the result won't be printed.
 
 def myfunction():
