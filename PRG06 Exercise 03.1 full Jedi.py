5b9ca8956863760688aefe875976fec585197b5b
Marieke vd Heuvel <92033155+DotsAndStripes@users.noreply.github.com>
2021-11-07 23:20:58 +0100

    Update PRG06 Exercise 03.1 full Jedi.py

--- PRG06 Exercise 03.1 full Jedi.py
+++ PRG06 Exercise 03.1 full Jedi.py
@@ -1,11 +1,11 @@
-# We went full Jedi on this one! The result cabn be found underneath!
+# We went full Jedi on this one! The result can be found underneath!
 def z(agevic,agemar,agefad,ageahm):
       a = [agevic,agemar,agefad,ageahm]
       return (sum(a)/(len(a)))
 #the array exist out of all the -agevic, agemar, etc- variables
 #the values will be obtained at a later stage (via the user). 
 agevic=int(input("enter the age Victor :"))
-agemar=int(input("enter the age Mariek :"))
+agemar=int(input("enter the age Marieke :"))
 agefad=int(input("enter the age Fadi :"))
 ageahm=int(input("enter the age Ahmed :"))
 #instead of receiving the values, we have chosen to receive the values via the PC user. The code will ask for input. 
