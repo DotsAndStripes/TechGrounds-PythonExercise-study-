dc9d3fc43cb414271f63eece12e7208c5bf17252
Marieke vd Heuvel <92033155+DotsAndStripes@users.noreply.github.com>
2021-11-07 22:18:08 +0100

    Update PRG04 Exercise 03.py

--- PRG04 Exercise 03.py
+++ PRG04 Exercise 03.py
@@ -1,3 +1,5 @@
+# As long as there is a variable/value in the list (arr), this variable/value will be printed.
+
 arr = ["Coen", "Casper", "Joshua", "Abdessamad", "Saskia"]
 for a in arr:
     print(a)
