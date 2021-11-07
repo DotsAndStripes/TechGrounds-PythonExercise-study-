38912282b90f17f78b274c4931d73f115eef85ef
Marieke vd Heuvel <92033155+DotsAndStripes@users.noreply.github.com>
2021-11-07 21:50:21 +0100

    Update and rename opdracht 4_1.py to opdracht 4 Exercise1.py

---
+++ opdracht 4 Exercise1.py
@@ -0,0 +1,6 @@
+# In this exercise, a simple while loop is used. 
+
+x = 0
+while x <= 10:
+  print(x)
+  x += 1
--- opdracht 4_1.py
+++
@@ -1,5 +0,0 @@
-# In this exercise, a simple while loop is used.  
-x = 0
-while x <= 10:
-  print(x)
-  x += 1
