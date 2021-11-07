2ae5d6d43d9e2abd99ba6e44a0c054ba1468881f
Marieke vd Heuvel <92033155+DotsAndStripes@users.noreply.github.com>
2021-11-07 23:12:27 +0100

    Add files via upload

---
+++ opdracht 6_2.py
@@ -0,0 +1,6 @@
+# With the command def, a new function is defined. 
+# The two () have to be written down behind myfunction, else the result won't be printed.
+
+def myfunction():
+    print("Hello World!")
+myfunction()
